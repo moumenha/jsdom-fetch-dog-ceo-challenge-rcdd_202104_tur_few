be4ed32499e0b91a41ffde1370f076e990295791
Moumen Hallak <moumen95@gmail.com>
2021-06-13 01:15:09 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+fetch(imgUrl)
+.then(response => response.json())
+.then(result => {
+  images(result.message);
+});
+
+  function images(dogsImages){
+    console.log(dogImages)
+    dogImages.forEach(dog => {
+      const container = document.querySelector('#dog-image-container')
+      const img = document.createElement('img')
+      img.src = dog
+      container.appendChild(img)
+    });
+  } 
