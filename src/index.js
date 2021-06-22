5b3c4c1be381a256ef7ce359342119c3363086b1
Moumen Hallak <moumen95@gmail.com>
2021-06-22 22:57:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,47 +1,17 @@
-document.addEventListener("DOMContentLoaded", (event) =>{
-
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-const breedsArr = []
 
-fetch(imgUrl)
-    .then(response=>response.json())
-    .then(json=>createImages(json))
+  fetch(imgUrl)
+  .then(response => response.json())
+  .then(result => {
+    dogImages(result.message)
+  })
 
-
-
-function createImages(json){
-    const container=document.querySelector("#dog-image-container")
-    json.message.forEach(function(dog){
-        const img=document.createElement("IMG")
-       img.src=dog
-        container.appendChild(img)
-    })
-}
-
-fetch(breedUrl)
-    .then(response=>response.json())
-    .then(json=>dogBreed(json))
-
-
-
-function dogBreed(json){
-    const breed=json.message
-    const list=document.querySelector("#dog-breeds")
-
-    Object.keys(breed).forEach(function(breedName) {
-        let li=document.createElement("li")
-        li.textContent=breedName
-        list.appendChild(li)
-    })
-
-}
-
-const ulList=document.querySelector("#dog-breeds")
-ulList.addEventListener("click",function(event){
-    event.target.style.color="blue"
-})
-
-
-
-}); 
+  function dogImages(dogs){
+    dogs.forEach(dog => {
+      const goodBoyContainer = document.querySelector('#dog-image-container')
+      const goodBoy = document.createElement('img')
+      goodBoy.src = dog
+      goodBoyContainer.appendChild(goodBoy)
+    });
+  } 
